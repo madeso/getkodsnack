7d17c036b5282e88a7f6b176ac8166cfb007e3b0
Gustav <sir.gustav.the.coder@gmail.com>
2014-11-04 14:35:17 +0100

    added more id3 tags

--- getkodsnack.py
+++ getkodsnack.py
@@ -2,13 +2,16 @@
 import re
 import os
 import os.path
+import eyed3
+# http://eyed3.nicfit.net/
+# pip install eyeD3 --allow-external eyed3 --allow-unverified eyed3
 
 def sanefilename(filename):
 	for c in r'[]/\;,><&*:%=+@!#^()|?^':
 		filename = filename.replace(c,'')
 	return filename
 
-def dlfile(url, localfilename):
+def dlfile(url, localfilename, date, title, num):
     # Open the url
 	try:
 		print "DL " + url + " to " + localfilename
@@ -17,6 +20,21 @@ def dlfile(url, localfilename):
 			f = urllib2.urlopen(url)
 			with open(localfilename, "wb") as local_file:
 				local_file.write(f.read())
+			audiofile = eyed3.load(localfilename)
+			
+			# http://www.richardfarrar.com/what-id3-tags-should-you-use-in-a-podcast/
+			audiofile.tag.artist = u"Kodsnack"
+			audiofile.tag.album = u"kodsnack.se"
+			audiofile.tag.album_artist = u""
+			audiofile.tag.title = title
+			audiofile.tag.genre = u'Speech' # or vocal, podcast isn't supported
+			audiofile.tag.date = date
+			audiofile.tag.year = date
+			audiofile.tag.www = u'www.kodsnack.se'
+			audiofile.tag.release_date = date
+			audiofile.tag.track_num = num
+
+			audiofile.tag.save()
 	#handle errors
 	except urllib2.HTTPError, e:
 		print "HTTP Error:", e.code, url
@@ -54,9 +72,16 @@ def main():
 			dodownload = False
 		#print(download.group(1))
 		
+		episodere = None
 		if dodownload:
+			episodere = re.search('[Kk]odsnack ([0-9]+)', title.group(1))
+			if episodere == None:
+				print "Unable to parse episode num"
+				dodownload = False
+		if dodownload:
+			episodenum = int(episodere.group(1))
 			# we should change to the same extension that the source has, someday...
-			dlfile(download.group(1), sanefilename(title.group(1))+ ".mp3")
+			dlfile(download.group(1), sanefilename(title.group(1))+ ".mp3", unicode(mo[0], 'CP1252'), unicode(title.group(1), 'CP1252'), episodenum)
 
 if __name__ == '__main__':
 	main()
